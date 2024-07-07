02afeb3fb3e117bb59774416d5539c3188206314
Aaron Smith <aaronsmith.dev@gmail.com>
2024-07-07 23:52:46 +0200

    Release disturbance post and randomly show suggestions at the bottom of posts

--- public/js/post.js
+++ public/js/post.js
@@ -11,22 +11,51 @@ const title = parser.parseFromString(`
   </div>
 `, 'text/html')
 
-const suggestions = parser.parseFromString(`
-  <div class="suggestions-container">
-    <div class="suggestions-title">If you liked this post, you may also like...</div>
-    <div class="suggestions-grid">
-      <a class="suggestion-color-1 suggestion" href="/blog/disturbance.html">
-        No Little Disturbance in Ephesus
-      </a>
-      <a class="suggestion-color-2 suggestion" href="/blog/torch.html">
-        Olympic Torch Relay in Montpellier
-      </a>
-      <a class="suggestion-color-3 suggestion" href="/blog/euchomai.html">
-        Euchomai
-      </a>
+const suggestablePosts = [
+  {
+    title: 'No Little Disturbance in Ephesus',
+    href: '/blog/disturbance.html',
+  },
+  {
+    title: 'Troy, circa 2024',
+    href: '/blog/troy.html',
+  },
+  {
+    title: 'Olympic Torch Relay in Montpellier',
+    href: '/blog/torch.html',
+  },
+  {
+    title: 'Euchomai',
+    href: '/blog/euchomai.html',
+  },
+]
+
+function createSuggestions() {
+  // Suggest suggestable posts other than the current one.
+  const suggestions = suggestablePosts
+    .filter(p => p.href != window.location.pathname)
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 3);
+  const randomColor = Math.floor(Math.random() * 2) + 1;
+
+  return parser.parseFromString(`
+    <div class="suggestions-container">
+      <div class="suggestions-title">If you liked this post, you may also like...</div>
+      <div class="suggestions-grid">
+        <a class="suggestion-color-${randomColor % 3 + 1} suggestion" href="${suggestions[0].href}">
+          ${suggestions[0].title}
+        </a>
+        <a class="suggestion-color-${(randomColor + 1) % 3 + 1} suggestion" href="${suggestions[1].href}">
+          ${suggestions[1].title}
+        </a>
+        <a class="suggestion-color-${(randomColor + 2) % 3 + 1} suggestion" href="${suggestions[2].href}">
+          ${suggestions[2].title}
+        </a>
+      </div>
     </div>
-  </div>
-`, 'text/html');
+  `, 'text/html');
+}
+
 
 document.getElementById('title').replaceWith(title.documentElement);
-document.getElementById('suggestions').replaceWith(suggestions.documentElement);
+document.getElementById('suggestions').replaceWith(createSuggestions().documentElement);
